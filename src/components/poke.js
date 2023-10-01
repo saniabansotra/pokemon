28db92efbcf806a95876eab98a0354e6dcd631eb
Sania Bansotra <saniabansotra@gmail.com>
2023-10-01 23:02:02 +0530

    message

--- src/components/poke.js
+++ src/components/poke.js
@@ -1,9 +1,31 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 const Poke = () => {
   const [num, setnum] = useState();
+  const [name, setname] = useState();
+  const [moves, setmoves] = useState();
+  useEffect(() => {
+    async function getData() {
+      const res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${num}`);
+      console.log(res);
+      setname(res.data.name);
+      setmoves(res.data.moves.length);
+    }
+    getData();
+  });
 
   return (
     <>
+      <h1> Hello Guys This is Pokemon</h1>
+      <h3>
+        Your Choose <span style={{ color: "Purple" }}> {num}</span> value
+      </h3>
+      <h3>
+        My name is <span style={{ color: "Purple" }}> {name}</span>{" "}
+      </h3>
+      <h3>
+        I have <span style={{ color: "Purple" }}> {moves} </span>moves
+      </h3>
       <select
         value={num}
         onChange={(event) => {
